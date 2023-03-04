1d97a92ed3e5d095c5de6361252a846f95f9fcb9
Andrey Supryaga <asupryaga@datamola.com>
2023-03-04 22:04:21 +0200

    Commit

--- lesson-9/5.insertion-and-removal/js.js
+++ lesson-9/5.insertion-and-removal/js.js
@@ -3,10 +3,13 @@
 const container = document.querySelector('.container');
 
 const node = document.createElement('div') || 'Hello world!';
+node.classList.add('message');
+node.innerHTML = 'Hello world!';
 
 container.append(node);       // вставляє в node, в кінець,
-container.prepend(node);      // вставляє в node, на початку,
-container.before(node);       // вставляє прямо перед node,
-container.after(node);        // вставляє відразу після node,
-container.replaceWith(node);  // замінює container.
-node.remove()                 // видаляє node.
+// container.prepend(node);      // вставляє в node, на початку,
+// container.before(node);       // вставляє прямо перед node,
+// container.after(node);        // вставляє відразу після node,
+// container.replaceWith(node);  // замінює container.
+
+// setTimeout(() => node.remove(), 2000);
