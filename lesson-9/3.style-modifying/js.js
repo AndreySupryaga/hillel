af016f357f07c64ce2dee396d4239007b5d9fa6b
Andrey Supryaga <asupryaga@datamola.com>
2023-03-05 15:02:25 +0200

    Commit

--- lesson-9/3.style-modifying/js.js
+++ lesson-9/3.style-modifying/js.js
@@ -2,8 +2,8 @@
 
 const msg = document.querySelector('.message');
 
-msg.setAttribute('style', 'color: red, flex: 1');
-msg.style.cssText = 'color: red; flex: 1';
-msg.style.backgroundColor = 'plum';
-msg.style.textTransform = 'uppercase';
-msg.style.flex = '1';
+msg.setAttribute('style', 'color: red; flex: 1');
+// msg.style.cssText = 'color: red; flex: 1';
+// msg.style.backgroundColor = 'plum';
+// msg.style.textTransform = 'uppercase';
+// msg.style.flex = '1';
