3cfb32ff72f76b6c277e0fa9ed4c627d4a5e1a91
Andrey Supryaga <asupryaga@datamola.com>
2023-03-05 11:50:18 +0200

    Commit

--- lesson-9/7.practice-procedure/js.js
+++ lesson-9/7.practice-procedure/js.js
@@ -1,9 +1,27 @@
 'use strict';
 
+/*
+    {
+        name: string,
+        message: string,
+        registered: string
+     }
+ */
 fetch('../mock-data.json')
     .then(response => response.json())
     .then(renderData);
 
+const tweetsContainer = document.querySelector('.messages-container');
 function renderData(data) {
-    console.log(data);
+    // data = Array(100000).fill( {
+    //     "name": "Coleen Coffey",
+    //     "message": "fugiat enim ad aute magna aute quis incididunt consectetur tempor culpa elit sint exercitation fugiat",
+    //     "registered": "2017-10-23"
+    // });
+    for (const item of data) {
+        const el = document.createElement('div');
+        el.classList.add('message');
+        el.innerHTML = item.message;
+        tweetsContainer.append(el);
+    }
 }
