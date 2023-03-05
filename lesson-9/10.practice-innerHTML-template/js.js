69065906ea38d949e44aed520f5cafcddda8617c
Andrey Supryaga <asupryaga@datamola.com>
2023-03-05 12:43:32 +0200

    Commit

--- lesson-9/10.practice-innerHTML-template/js.js
+++ lesson-9/10.practice-innerHTML-template/js.js
@@ -11,10 +11,19 @@ fetch('../mock-data.json')
     .then(response => response.json())
     .then(renderData);
 
+const messageTpl = `<div class="message">
+            <span class="author">{author}</span>
+            <span class="time">{time}</span>
+            <span class="text">{message}</span>
+        </div>`;
+
 const tweetsContainer = document.querySelector('.messages-container');
 
 function renderData(data) {
     tweetsContainer.innerHTML = data.map(item =>
-        `<div class="message">${item.message}</div>`
+        messageTpl
+            .replace('{author}', item.author)
+            .replace('{time}', item.time)
+            .replace('{message}', item.message)
     ).join('');
 }
