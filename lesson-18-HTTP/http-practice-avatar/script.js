3f1ea296cde0fb003f5894fd2867af263e910751
Andrey Supryaga <asupryaga@datamola.com>
2023-04-09 18:48:47 +0300

    Commit

--- lesson-18-HTTP/http-practice-avatar/script.js
+++ lesson-18-HTTP/http-practice-avatar/script.js
@@ -1,8 +1,9 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19')
-    .then((data) => data.json)
-    .then(console.log);
+fetch('https://api.github.com/users/AndreySupryaga')
+    .then((data) => data.json())
+    .then(handleResponse);
 
 function handleResponse(data) {
+    console.log(data);
     const img = document.createElement('img');
     img.setAttribute('src', data.avatar_url);
     document.body.prepend(img);
