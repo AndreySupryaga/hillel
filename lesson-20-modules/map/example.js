ac38729e79988488928155acc482a229b0910696
Andrey Supryaga <asupryaga@datamola.com>
2023-04-13 19:47:07 +0300

    Commit

--- lesson-20-modules/map/example.js
+++ lesson-20-modules/map/example.js
@@ -7,3 +7,13 @@ const entries = Object.entries(obj);
 const map = new Map(entries);
 const objFromMap = Object.fromEntries([...map]);
 console.log(objFromMap);
+
+const f = () => {};
+
+const map1 = new Map();
+
+map1.set(true, 1);
+map1.set(f, 'Function value');
+
+console.log(map1.get(true));
+console.log(map1.get(f));
